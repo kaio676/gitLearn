c99b86469fe647cdab6eb39ca8064a362d5489e1
Kaio Alves <kaioalves676@gmail.com>
2019-02-10 17:24:38 -0200

    Projeto iniciado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
